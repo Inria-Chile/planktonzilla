7f0633b65a02b3f1ddfd320ec5c0450c4819c799
L.M. <noreply@github.com>
2025-10-21 19:54:38 +0200

    update slurm multiple gpus

--- planktonzilla/train_node.py
+++ planktonzilla/train_node.py
@@ -1,9 +1,15 @@
 """
-(c) Inria
+Prueba de ejecución Hydra + Submitit (Jean Zay)
 """
 
+import os
+import socket
+import datetime
 import pyrootutils
+from omegaconf import DictConfig
+import hydra
 
+# --- Configuración raíz del proyecto ---
 root = pyrootutils.setup_root(
     search_from=__file__,
     indicator=[".git", "pyproject.toml"],
@@ -11,21 +17,10 @@ root = pyrootutils.setup_root(
     dotenv=True,
 )
 
-from omegaconf import DictConfig
-import hydra
-from planktonzilla.utils.hydra import (
-    get_metric_value,
-    task_wrapper,
-)
-
-
 @hydra.main(version_base="1.3", config_path=str(root / "configs"), config_name="train.yaml")
-def main(cfg: DictConfig) -> float | None:
-    metric_dict, _ = train(cfg)
-
-    metric_value = get_metric_value(metric_dict=metric_dict, metric_name=cfg.get("optimized_metric"))
-
-    return metric_value
+def main(cfg: DictConfig):
+    print("\n" + "AAAAAAAAA")
+    print("\n" + "AAAAAAAAA")
 
 
 if __name__ == "__main__":
